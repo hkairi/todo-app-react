2aba414add33a85ffdf1c79ffb6071eabd532a0c
Hassane Moustapha <hassanemoustapha@gmail.com>
2015-01-04 10:55:02 +0000

    basic structure for the app

---
+++ js/src/app.js
@@ -0,0 +1,6 @@
+var React   = require('react');
+var TodoApp = require('./components/todoapp');
+
+React.render(
+  <TodoApp />, document.getElementById('main')
+);
---
+++ js/src/components/todoapp.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var TodoApp = React.createClass({
+  render: function(){
+    return(
+      <div className='main-app'>
+        <h1> ici l'application quoi</h1>
+      </div>
+    );
+  }
+});
+
+module.exports = TodoApp;
