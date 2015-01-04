ae512eb967e00e530defe95e489c15ad80b5cbae
Hassane Moustapha <hassanemoustapha@gmail.com>
2015-01-04 11:28:03 +0000

    adding the new todo to the todos collection

--- js/src/components/todoapp.js
+++ js/src/components/todoapp.js
@@ -1,9 +1,30 @@
 var React = require('react');
 
 var TodoApp = React.createClass({
+  getInitialState: function(){
+    return {
+      todos : []
+    };
+  },
+
   onClick: function(){
-    var _todo =  this.refs.todo.getDOMNode().value;
-    console.log("ok", _todo);
+    var _todo = this.refs.todo.getDOMNode();
+    var _todos= this.state.todos;
+    _todos.push({ texte: _todo.value, done: false });
+
+    _todo.value = '';
+
+    this.setState({
+      todos: _todos
+    });
+  },
+
+  showTodos: function(){
+    return(
+      this.state.todos.map(function(t){
+        return <p key={this.state.todos.indexOf(t)}>{t.texte}</p>
+      }.bind(this))
+    );
   },
 
   render: function(){
@@ -12,6 +33,10 @@ var TodoApp = React.createClass({
         <h1> ici l'application quoi</h1>
         <input ref='todo' type="text" placeholder="something to do ?" />
         <button onClick={this.onClick}>Ok</button>
+        <hr />
+        todos : {this.state.todos.length}
+        <hr />
+        {this.showTodos()}
       </div>
     );
   }
