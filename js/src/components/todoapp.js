7a8c582b43db7c474416c7e394f2d0ce8e84a483
Hassane Moustapha <hassanemoustapha@gmail.com>
2015-01-04 11:07:57 +0000

    added an input to enter todos

--- js/src/components/todoapp.js
+++ js/src/components/todoapp.js
@@ -1,10 +1,17 @@
 var React = require('react');
 
 var TodoApp = React.createClass({
+  onClick: function(){
+    var _todo =  this.refs.todo.getDOMNode().value;
+    console.log("ok", _todo);
+  },
+
   render: function(){
     return(
       <div className='main-app'>
         <h1> ici l'application quoi</h1>
+        <input ref='todo' type="text" placeholder="something to do ?" />
+        <button onClick={this.onClick}>Ok</button>
       </div>
     );
   }
